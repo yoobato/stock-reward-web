6ec5c237ba141b689c34906f9716ba7dc0ccabdd
Daeyeol Ryu <dy@yoobato.com>
2019-11-23 14:25:48 +0900

    Web page skeleton

--- routes/web.js
+++ routes/web.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // TODO: Test
+  // TODO: 상품 목록
   res.render('index', {
     length: 10,
+  });
+});
+
+router.get('/detail', (req, res) => {
+  // TODO: 상품 상세
+  res.render('detail', {
+    length: 5,
   });
 });
 
