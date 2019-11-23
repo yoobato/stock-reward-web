2646b3081d8287f92478b77f052c21e45a932b57
Daeyeol Ryu <dy@yoobato.com>
2019-11-23 16:50:50 +0900

    Remove useless code

--- models/User.js
+++ models/User.js
@@ -1,21 +1,6 @@
 const MariaDB = require('../database/MariaDB');
 
 class User {
-  getUser(userId) {
-    return new Promise((resolve, reject) => {
-      console.log('[유저] 정보 조회');
-
-      return MariaDB.query(`SELECT * FROM user WHERE id = ${userId}`).then(({ client, res }) => {
-        MariaDB.close(client);
-        return resolve(res);
-      }).catch((err) => {
-        console.log(`[유저] 정보 조회 오류: ${err}`);
-        MariaDB.close(client);
-        return reject(err);
-      });
-    });
-  }
-
   addStock(userId, storeId, stock, amount) {
     return new Promise((resolve, reject) => {
       console.log(`[유저] 주식 지급`);
